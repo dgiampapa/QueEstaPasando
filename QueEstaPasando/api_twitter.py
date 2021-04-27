a6966938044a1cfb1dde0a2048365ed82b266095
Antonio Daniel Giampapa <daniel.giampapa@gmail.com>
2021-04-27 14:30:29 -0400

    Ultima version

--- QueEstaPasando/api_twitter.py
+++ QueEstaPasando/api_twitter.py
@@ -11,17 +11,20 @@ def buscar_tweets_mentions(mentions_id, cantidad_tweet=400):
     data = pd.DataFrame()
     params = {
         'tweet.fields': 'created_at,author_id',
-        'max_results':100} 
+        'max_results':200} 
     
-    for cantidad in range(int(cantidad_tweet/100)):
+    for cantidad in range(int(cantidad_tweet/200)):
             respuesta = requests.get(url, params=params, headers=headers).json()
             data = data.append(respuesta['data'])
             params = {
                 'tweet.fields': 'created_at,author_id',
                 'pagination_token': respuesta['meta']['next_token'],
-                'max_results':100} 
+                'max_results':200} 
     
     return data
 
 def grabar_tweets(df, ruta_archivo):
     df.to_csv(ruta_archivo)
+
+def leer_tweets(ruta_archivo):
+    return pd.read_csv(ruta_archivo, index_col=0)      
